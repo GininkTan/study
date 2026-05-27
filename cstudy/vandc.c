f135e01e11728472934fb2e438d5de9a62aef9e9
GininkTan <bailu1c3@foxmail.com>
2026-05-27 16:47:53 +0800

    '5.27'

---
+++ cstudy/vandc.c
@@ -0,0 +1,25 @@
+//  变量和常量
+// 方式1
+/*
+int age  ;
+char eva ;
+age = 24 ;
+eva= 'A' ;
+*/
+//方式2
+/*
+int age =25 ;
+char eva='A' ;
+*/
+// 方式3
+/*
+double height ,weight ;
+height =180.5 ; 
+weight = 75.5 ;
+*/
+
+// 方式4
+/*
+double height =180.5,weight;
+weight =75.5;
+*/
